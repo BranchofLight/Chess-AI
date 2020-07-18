4d6572f1b0656425452b16a7c9e2bf1614ca87eb
Abe Fehr <abe.fehr@gmail.com>
2020-07-18 16:13:13 -0400

    Add prettier

--- src/common/init_board.js
+++ src/common/init_board.js
@@ -1,25 +1,35 @@
-import { COLOUR, PIECE_CHAR } from './constants.js';
+import { COLOUR, PIECE_CHAR } from "./constants.js";
 
 const b = (c, p) => {
   return {
     colour: c,
     piece: p,
-    hasMoved: false
+    hasMoved: false,
   };
 };
 
 const firstRank = [
-  PIECE_CHAR.ROOK, PIECE_CHAR.BISHOP, PIECE_CHAR.KNIGHT, PIECE_CHAR.QUEEN,
-  PIECE_CHAR.KING, PIECE_CHAR.KNIGHT, PIECE_CHAR.BISHOP, PIECE_CHAR.ROOK
+  PIECE_CHAR.ROOK,
+  PIECE_CHAR.BISHOP,
+  PIECE_CHAR.KNIGHT,
+  PIECE_CHAR.QUEEN,
+  PIECE_CHAR.KING,
+  PIECE_CHAR.KNIGHT,
+  PIECE_CHAR.BISHOP,
+  PIECE_CHAR.ROOK,
 ];
 
 export const INIT_BOARD = [
-  firstRank.map(p => b(COLOUR.BLACK, p)),
-  Array(8).fill({}).map(a => b(COLOUR.BLACK, PIECE_CHAR.PAWN)),
+  firstRank.map((p) => b(COLOUR.BLACK, p)),
+  Array(8)
+    .fill({})
+    .map((a) => b(COLOUR.BLACK, PIECE_CHAR.PAWN)),
   Array(8).fill({ piece: PIECE_CHAR.EMPTY }),
   Array(8).fill({ piece: PIECE_CHAR.EMPTY }),
   Array(8).fill({ piece: PIECE_CHAR.EMPTY }),
   Array(8).fill({ piece: PIECE_CHAR.EMPTY }),
-  Array(8).fill({}).map(a => b(COLOUR.WHITE, PIECE_CHAR.PAWN)),
-  firstRank.map(p => b(COLOUR.WHITE, p))
+  Array(8)
+    .fill({})
+    .map((a) => b(COLOUR.WHITE, PIECE_CHAR.PAWN)),
+  firstRank.map((p) => b(COLOUR.WHITE, p)),
 ];
